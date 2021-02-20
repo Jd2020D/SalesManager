052644b5e05d6f9b6ffb6b4c60c3b14516577f69
NoorZak <n.zakarneh1@student.aaup.edu>
2021-02-20 02:53:09 +0200

    fridge cont

--- server/controllers/fridge.controller.js
+++ server/controllers/fridge.controller.js
@@ -6,3 +6,36 @@ module.exports.createFridge = (request, response) => {
         .catch(err => response.status(400).json(err));
 }
 
+
+
+module.exports.getAllFridges = (_req, res) => {
+    Fridge.find()
+        .then(fridges => res.json(fridges))
+        .catch(err => res.json(err));
+}
+
+
+
+
+module.exports.findSingleFridge = (req, res) => {
+    const {id} = req.params;
+    Fridge.findById(id)
+        .then(fridge => res.json(fridge))
+        .catch(err => res.json(err));
+}
+
+
+module.exports.deleteFridge = (req, res) => {
+    const {id} = req.params;
+    Fridge.findByIdAndDelete(id)
+        .then(() => res.json({success: true}))
+        .catch(err => res.json(err));
+}
+
+module.exports.updateFridge = (req, res) => {
+    const {id} = req.params;
+    const {serialNumber} = req.body;
+    Fridge.findByIdAndUpdate(id, {serialNumber: serialNumber}, {new: true, runValidators: true})
+        .then(updated => res.json(updated))
+        .catch(err => res.status(400).json(err));
+}
--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -6,7 +6,7 @@ const { request, response } = require('express');
 require('dotenv').config();
 
 module.exports.getSingleUserById=(request, response) => {
-     response.json(response.locals.user);
+    response.json(response.locals.user);
 }
 
 
@@ -33,144 +33,144 @@ module.exports.getSingleUserById=(request, response) => {
 
 module.exports.register =  (req, res) => {
     User.create(req.body)
-      .then(user => {
-        const userToken = jwt.sign({
-              id: user._id
-          }, process.env.SECRET_KEY);
+        .then(user => {
+            const userToken = jwt.sign({
+                id: user._id
+            }, process.env.SECRET_KEY);
 
-        res
-            .cookie("usertoken", userToken, {
-                httpOnly: true
-            })
-            .json({ msg: "success!", user: user });
+            res
+                .cookie("usertoken", userToken, {
+                    httpOnly: true
+                })
+                .json({ msg: "success!", user: user });
 
-      })
-      .catch(err => res.status(400).json(err));
-  }
+        })
+        .catch(err => res.status(400).json(err));
+}
 
 //   module.exports.login= async(req, res) => {
 //         const user = await User.findOne({ email: req.body.email });
-     
+
 //         if(user === null) {
 //             // email not found in users collection
 //             return res.sendStatus(400);
 //         }
-     
+
 //         // if we made it this far, we found a user with this email address
 //         // let's compare the supplied password to the hashed password in the database
 //         const correctPassword = await bcrypt.compare(req.body.password, user.password);
-     
+
 //         if(!correctPassword) {
 //             // password wasn't a match!
 //             return res.sendStatus(400);
 //         }
-     
+
 //         // if we made it this far, the password was correct
 //         const userToken = jwt.sign({
 //             id: user._id
 //         }, process.env.SECRET_KEY);
-     
+
 //         // note that the response object allows chained calls to cookie and json
 //         res.cookie("usertoken", userToken,  {
 //                 httpOnly: true
 //             })
 //             .json({ msg: "success!" ,user:user});
 //     }
-    module.exports.login= async(req, res) => {
-            let errors={};
-            const user = await User.findOne({ email: req.body.email })
-            .then(async user=>{
-                if(!req.body.email)
-                    errors= {...errors,
-                        email:{
-                            kind: "required",
+module.exports.login= async(req, res) => {
+    let errors={};
+    const user = await User.findOne({ email: req.body.email })
+        .then(async user=>{
+            if(!req.body.email)
+                errors= {...errors,
+                    email:{
+                        kind: "required",
+                        message: "Email is required",
+                        name: "ValidatorError",
+                        path: "email",
+                        properties:{
                             message: "Email is required",
-                            name: "ValidatorError",
                             path: "email",
-                            properties:{
-                                message: "Email is required",
-                                path: "email",
-                                type: "required",
-                                value: req.body.email   
-                            },
+                            type: "required",
                             value: req.body.email
-                        }
+                        },
+                        value: req.body.email
+                    }
                 };
-                if(!req.body.password)
-                    errors= {...errors,
-                        password:{
-                            kind: "required",
+            if(!req.body.password)
+                errors= {...errors,
+                    password:{
+                        kind: "required",
+                        message: "Password is required",
+                        name: "ValidatorError",
+                        path: "password",
+                        properties:{
                             message: "Password is required",
-                            name: "ValidatorError",
                             path: "password",
-                            properties:{
-                                message: "Password is required",
-                                path: "password",
-                                type: "required",
-                                value: ''   
-                            },
+                            type: "required",
                             value: ''
-                        }
+                        },
+                        value: ''
+                    }
                 };
-                if (Object.keys(errors).length>0)
-                    throw errors;
-                if(user === null) 
-                    throw {...errors,
-                        email:{
-                            kind: "invalid",
+            if (Object.keys(errors).length>0)
+                throw errors;
+            if(user === null)
+                throw {...errors,
+                    email:{
+                        kind: "invalid",
+                        message: "Email isnt valid",
+                        name: "ValidatorError",
+                        path: "email",
+                        properties:{
                             message: "Email isnt valid",
-                            name: "ValidatorError",
                             path: "email",
-                            properties:{
-                                message: "Email isnt valid",
-                                path: "email",
-                                type: "invalid",
-                                value: req.body.email   
-                            },
+                            type: "invalid",
                             value: req.body.email
-                        }
-                    };
-            const correctPassword = await bcrypt.compare(req.body.password, user.password);
-                    if(!correctPassword) 
-                        throw  {...errors,
-                            password:{
-                                kind: "invalid",
-                                message: "Password isnt valid",
-                                name: "ValidatorError",
-                                path: "password",
-                                properties:{
-                                    message: "Password isnt valid",
-                                    path: "password",
-                                    type: "invalid",
-                                    value: ''   
-                                },
-                                value: ''
-                            }    
-                        }
-                            // if we made it this far, the password was correct
-                    const userToken = jwt.sign({
-                        id: user._id
-                    }, process.env.SECRET_KEY);
-                    
-                    // note that the response object allows chained calls to cookie and json
-                    res.cookie("usertoken", userToken,  {
-                            httpOnly: true
-                        })
-                        .json({ msg: "success!" ,user:user});
-                
+                        },
+                        value: req.body.email
+                    }
+                };
+            const correctPassword = await bcrypt.compare(req.body.password, user.password);
+            if(!correctPassword)
+                throw  {...errors,
+                    password:{
+                        kind: "invalid",
+                        message: "Password isnt valid",
+                        name: "ValidatorError",
+                        path: "password",
+                        properties:{
+                            message: "Password isnt valid",
+                            path: "password",
+                            type: "invalid",
+                            value: ''
+                        },
+                        value: ''
+                    }
+                }
+            // if we made it this far, the password was correct
+            const userToken = jwt.sign({
+                id: user._id
+            }, process.env.SECRET_KEY);
+
+            // note that the response object allows chained calls to cookie and json
+            res.cookie("usertoken", userToken,  {
+                httpOnly: true
             })
-            .catch(err=>{
-                return res.status(400).json({errors:err});
-            });
-         
-         
-         
-        }
-    module.exports.logout= (req, res) => {
-        res.clearCookie('usertoken');
-        res.sendStatus(200);
-    }
-    
+                .json({ msg: "success!" ,user:user});
+
+        })
+        .catch(err=>{
+            return res.status(400).json({errors:err});
+        });
+
+
+
+}
+module.exports.logout= (req, res) => {
+    res.clearCookie('usertoken');
+    res.sendStatus(200);
+}
+
 // module.exports.findAllUsers = (request, response) => {
 //     User.find({})
 //         .then(allUsers => response.json(allUsers))
